46e471b3b90f9cb15e685a087bd68b8167bb0028
viktorkaramba <svynarvikror@gmail.com>
2021-02-23 20:39:38 +0200

    Add new test cases for functions

--- Laboratory_1/Main.cpp
+++ Laboratory_1/Main.cpp
@@ -34,3 +34,41 @@ TEST_CASE("Test 4(old) Check Matrix function") {
 	CHECK(M1.Value_Search(4)  == a);
 	CHECK(M1.Index_Search(0, 1) == b);
 }
+TEST_CASE("Test 5(new) Check SumMatrix ") {
+	Figure A(3, { 0, 0, 5 }, { 0, 2, 0 });
+	Figure B(4, { 2, 5, 3, 7 }, { 4, 8, 5, 2 });
+	Figure C(5, { 1, -3, 3,-2,-1 }, { 2, -2, 4,0, 3 });
+	Matrix M1(2, 3, { A,C,B }, { 0,1,1 }, { 0, 1, 2 });
+	Matrix M2(2, 3, { A,B,C }, { 0,1,1 }, { 0, 1, 2 });
+	Matrix M3;
+	M3.SumMatrix(M1.matrix, M2.matrix, 2, 3);
+	CHECK(M3.matrix[0]->square == 10);
+}
+TEST_CASE("Test 6(new) Check SumMatrix ") {
+	Figure A(3, { 1, 3, 5 }, { 1, 5, 2 });
+	Figure B(4, { 0, 0, 5, 5 }, { 0, 5, 0, 5 });
+	Figure C(5, { 1, -3, 3,-2,-1 }, { 2, -2, 4,0, 3 });
+	Matrix M1(2, 3, { A,C,B }, { 0,1,1 }, { 0, 0, 1 });
+	Matrix M2(2, 3, { A,B,C }, { 0,1,1 }, { 0, 0, 1 });
+	Matrix M3;
+	M3.SumMatrix(M1.matrix, M2.matrix, 2, 3);
+	CHECK(M3.matrix[0]->square == 14);
+}
+TEST_CASE("Test 7(new) Check MultiplyVector") {
+	Figure A(3, { 0, 0, 5 }, { 0, 2, 0 });
+	Figure B(4, { 2, 5, 3, 7 }, { 4, 8, 5, 2 });
+	Figure C(5, { 1, -3, 3,-2,-1 }, { 2, -2, 4,0, 3 });
+	Matrix M1(2, 3, { A,C,B }, { 0,1,1 }, { 0, 1, 2 });
+	Matrix M3;
+	M3.MultiplyVector(M1.matrix, { 1,2,3 }, 2, 3);
+	CHECK(M3.matrix[1]->square == 18);
+}
+TEST_CASE("Test 8(new) Check MultiplyVector") {
+	Figure A(3, { 1, 3, 5 }, { 1, 5, 2 });
+	Figure B(4, { 0, 0, 5, 5 }, { 0, 5, 0, 5 });
+	Figure C(5, { 1, -3, 3,-2,-1 }, { 2, -2, 4,0, 3 });
+	Matrix M1(2, 3, { A,C,B }, { 0,1,1 }, { 0, 0, 1 });
+	Matrix M3;
+	M3.MultiplyVector(M1.matrix, { 3, 2, 1 }, 2, 3);
+	CHECK(M3.matrix[0]->square == 21);
+}
